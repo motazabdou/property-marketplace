78e2fd4ffe4442b5fc509b65749dc578fa299ff7
Motaz Abdou <moutaz30@hotmail.com>
2022-08-08 18:29:06 +0100

    Added firebase configuration

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,11 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyByxW4pOr31M1U1Zh6Il-vuxDJveEFGxeo",
   authDomain: "property-marketplace-app-79db9.firebaseapp.com",
